afaa5112941290966e13afcd1d9a42d45f573ed3
Xinyao Wang <xinyao.wang@anu.edu.au>
2020-03-28 16:04:41 +1100

    change name of placement

---
+++ src/comp1110/ass2/VerifyPlacement.java
@@ -0,0 +1,19 @@
+package comp1110.ass2;
+
+public class VerifyPlacement {
+    public static boolean isPiecePlacementWellFormed(String piecePlacement) {
+        // FIXME Task 2: determine whether a piece placement is well-formed判断一个图块的放置是不是well-formed
+        return false;
+    }
+
+    public static boolean isPlacementSequenceWellFormed(String placement) {
+        // FIXME Task 3: determine whether a placement sequence is well-formed
+        return false;
+    }
+
+    public static boolean isPlacementSequenceValid(String placementSequence) {
+        // FIXME Task 6: determine whether a placement sequence is valid
+        return false;
+    }
+    //this class is to check the place of the tiles cannot be overlap
+}
