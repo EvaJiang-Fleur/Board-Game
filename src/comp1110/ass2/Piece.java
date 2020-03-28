fe90fb3926244de0a6fc582dbd2532e1385930d0
Xinyi Zhang <u6976740@anu.edu.au>
2020-03-28 16:44:15 +1100

    add some new things

--- src/comp1110/ass2/Piece.java
+++ src/comp1110/ass2/Piece.java
@@ -3,5 +3,32 @@ package comp1110.ass2;
 public class Piece {
     //this class is used to place the pieces into the board
     //the string of the tiles corresponding the type of each tiles and placed them into the right corresponding coordinate
+    private String tile;
+    private int x;
+    private int y;
+    private String tileplacement;
+
+    public Piece(String tileplacement) {
+        this.tile = tileplacement.substring(0,3);
+        this.x = tileplacement.charAt(4) - '0';
+        this.y = tileplacement.charAt(5) - '0';
+
+
+    }
+
+
+    public String getTile() {
+        return tile;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+
+    public int getY() {
+        return y;
+    }
+
 
 }
