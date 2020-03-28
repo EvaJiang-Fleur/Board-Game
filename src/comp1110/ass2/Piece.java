ca2cd6b96fd419ea56022f2a654bfcf89f836d12
Xinyi Zhang <u6976740@anu.edu.au>
2020-03-28 16:37:37 +1100

    Verify placement

--- src/comp1110/ass2/Piece.java
+++ src/comp1110/ass2/Piece.java
@@ -2,6 +2,6 @@ package comp1110.ass2;
 
 public class Piece {
     //this class is used to place the pieces into the board
-    //the string of the tiles corresponding the type of each tiles and placed them into the corresponding coordinate
+    //the string of the tiles corresponding the type of each tiles and placed them into the right corresponding coordinate
 
 }
--- src/comp1110/ass2/VerifyPlacement.java
+++ src/comp1110/ass2/VerifyPlacement.java
@@ -3,6 +3,7 @@ package comp1110.ass2;
 public class VerifyPlacement {
     //This class checks whether the place of each pieces is overlap
     //this class can also check each tiles is in the board or not(the tiles cannot be out of the boundaries)
+
     public static boolean isPiecePlacementWellFormed(String piecePlacement) {
         // FIXME Task 2: determine whether a piece placement is well-formed
         return false;
