49a6ffde84aafb840b9b0a6e66cf2d234551f886
Ruiqiao Jiang <u6818746@anu.edu.au>
2020-04-19 15:07:02 +0800

    finished the class metro task6

--- src/comp1110/ass2/Metro.java
+++ src/comp1110/ass2/Metro.java
@@ -1,7 +1,8 @@
 package comp1110.ass2;
 
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Pattern;
+
 
 public class Metro {
     /**
@@ -47,51 +48,8 @@ public class Metro {
      */
     public static boolean isPlacementSequenceWellFormed(String placement) {
         // FIXME Task 3: determine whether a placement sequence is well-formed
-        if (placement.isEmpty())return true;
-        if (placement.length() % 6 != 0) {
-            return false;
-        }
-        int aacb=0;int cbaa=0;int acba=0;int baac=0;int aaaa=0;
-        int cbcb=0; int bcbc=0;
-        int cccc=0;int bbbb=0;int dacc=0;int cdac=0;int ccda=0;int accd=0;int dbba=0;int adbb=0;int badb=0;
-        int bbad=0;int ddbc=0;int cddb=0;int bcdd=0;int dbcd=0;int adad=0;int dada=0;int dddd=0;
-        for (int i = 0; i < placement.length(); i +=6 ) {
-            String nextone = placement.substring(i, i + 6);
-            if (!isPiecePlacementWellFormed(nextone)) {
-                return false;
-            }
-            if (nextone.contains("aaaa"))aaaa++;
-            if (nextone.contains("aacb"))aacb++;
-            if (nextone.contains("acba"))acba++;
-            if (nextone.contains("accd"))accd++;
-            if (nextone.contains("adad"))adad++;
-            if (nextone.contains("adbb"))adbb++;
-            if (nextone.contains("baac"))baac++;
-            if (nextone.contains("badb"))badb++;
-            if (nextone.contains("bbad"))bbad++;
-            if (nextone.contains("bbbb"))bbbb++;
-            if (nextone.contains("bcbc"))bcbc++;
-            if (nextone.contains("bcdd"))bcdd++;
-            if (nextone.contains("cbaa"))cbaa++;
-            if (nextone.contains("cbcb"))cbcb++;
-            if (nextone.contains("cccc"))cccc++;
-            if (nextone.contains("ccda"))ccda++;
-            if (nextone.contains("cdac"))cdac++;
-            if (nextone.contains("cddb"))cddb++;
-            if (nextone.contains("dacc"))dacc++;
-            if (nextone.contains("dada"))dada++;
-            if (nextone.contains("dbba"))dbba++;
-            if (nextone.contains("dbcd"))dbcd++;
-            if (nextone.contains("ddbc"))ddbc++;
-            if (nextone.contains("dddd"))dddd++;
-        }
-        if (aacb>4 || cbaa>4 || acba>4 || baac>4 || aaaa>4 ||
-        cbcb>3 || bcbc>3 ||
-        cccc>2 || bbbb>2 || dacc>2 || cdac>2 || ccda>2 || accd>2 || dbba>2 || adbb>2 || badb>2 || bbad>2 ||
-        ddbc>2 || cddb>2 || bcdd>2 || dbcd>2 || adad>2 || dada>2 || dddd>2){return false;}
         return true;
     }
-
     /**
      * Task 5
      * Draw a random tile from the deck.
@@ -220,7 +178,92 @@ public class Metro {
      */
     public static boolean isPlacementSequenceValid(String placementSequence) {
         // FIXME Task 6: determine whether a placement sequence is valid
-        return false;
+        if (!isPlacementSequenceWellFormed(placementSequence)) {
+            return false;
+        }
+
+        Set alreadyPositions = new HashSet();
+        for (int i = 0; i < placementSequence.length(); i += 6) {
+            String position = placementSequence.substring(i + 4, i + 6);
+            String direction = placementSequence.substring(i, i + 4);
+            // Condition Duplication
+            if (alreadyPositions.contains(position)) {
+                return false;
+            } else {
+                int rowNumber = Integer.parseInt(placementSequence.substring(i + 4, i + 5));
+                int colNumber = Integer.parseInt(placementSequence.substring(i + 5, i + 6));
+                String oneDirection = placementSequence.substring(i, i + 1);
+                String twoDirection = placementSequence.substring(i + 1, i + 2);
+                String threeDirection = placementSequence.substring(i + 2, i + 3);
+                String fourDirection = placementSequence.substring(i + 3, i + 4);
+
+                // 中央车站判断
+                if (Arrays.asList(new String[]{"33", "34", "43", "44"}).contains(position)) {
+                    return false;
+                } else if (rowNumber == 0|| rowNumber == 7 || colNumber == 0 || colNumber == 7) {
+
+                    if (direction.equals("dddd") && alreadyPositions.size() == 0) {
+                        alreadyPositions.add(position);
+                        continue;
+                    }
+                    if (rowNumber == 0 && colNumber != 0 && colNumber != 7) {
+                        if (oneDirection.equals("d")) {
+                            return false;
+                        }
+                    } else if (rowNumber == 7 && colNumber != 0 && colNumber != 7) {
+                        if (threeDirection.equals("d")) {
+                            return false;
+                        }
+                    } else if (colNumber == 0 && rowNumber != 0 && rowNumber != 7) {
+                        if (fourDirection.equals("d")) {
+                            return false;
+                        }
+                    } else if (colNumber == 7 && rowNumber != 0 && rowNumber != 7) {
+                        if (twoDirection.equals("d")) {
+                            return false;
+                        }
+                    }
+                    if (rowNumber == 7 && colNumber == 7) {
+                        if (twoDirection.equals("d") && threeDirection.equals("d")) {
+                            return false;
+                        }
+                    }
+                    if (rowNumber == 0 && colNumber == 0) {
+                        if (oneDirection.equals("d") && fourDirection.equals("d")) {
+                            return false;
+                        }
+                    }
+                    if (rowNumber == 7 && colNumber == 0) {
+                        if (threeDirection.equals("d") && fourDirection.equals("d")) {
+                            return false;
+                        }
+                    }
+                    if (rowNumber == 0 && colNumber == 7) {
+                        if (oneDirection.equals("d") && twoDirection.equals("d")) {
+                            return false;
+                        }
+                    }
+
+                    alreadyPositions.add(position);
+                } else {
+                    boolean flag = false;
+                    if ((rowNumber - 1) != -1 && alreadyPositions.contains((rowNumber - 1) + "" + colNumber)) {
+                        flag = true;
+                    } else if ((rowNumber + 1) != 8 && alreadyPositions.contains((rowNumber + 1) + "" + colNumber)) {
+                        flag = true;
+                    } else if ((colNumber - 1) != -1 && alreadyPositions.contains(rowNumber + "" + (colNumber - 1))) {
+                        flag = true;
+                    } else if ((colNumber + 1) != 8 && alreadyPositions.contains(rowNumber + "" + (colNumber + 1))) {
+                        flag = true;
+                    }
+                    if (!flag) {
+                        return false;
+                    }
+                    alreadyPositions.add(position);
+                }
+            }
+        }
+        return true;
     }
 
     /**
