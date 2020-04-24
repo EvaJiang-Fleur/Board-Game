3dca19e2ac15410034e10b1e41a68f0f21002bec
Xinyi Zhang <u6976740@anu.edu.au>
2020-04-24 21:51:22 +1000

    originality/and some new changes

--- src/comp1110/ass2/gui/Menu.java
+++ src/comp1110/ass2/gui/Menu.java
@@ -1,14 +1,27 @@
 package comp1110.ass2.gui;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 
-public class Menu {
+
+
+public class Menu extends Application {
+    private static final int WIDTH = 1024;
+    private static final int HEIGHT = 768;
     //add the background of the game into the interface of the manu
     private void background(){
-
+        ImageView imageView = new ImageView(new Image(getClass().getResource("background.jpg").toExternalForm()));
+        imageView.setFitWidth(WIDTH);
+        imageView.setFitHeight(HEIGHT);
+        //getChildren().add(imageView);
     }
 
     //the title of the game:Metro
     private void title(){
+       // Title title = new Title("Metro: Queen games");
+
 
     }
 
@@ -16,5 +29,9 @@ public class Menu {
 
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
 
--- src/comp1110/ass2/gui/Title.java
+++
@@ -1,5 +0,0 @@
-package comp1110.ass2.gui;
-
-public class Title {
-    //this class is used for create the title of the game in the manu interface.
-}
